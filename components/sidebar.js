fc2813836218379deb5c04e4da88d4bf7e1432ca
Sankalp Jaiswal <sankalpjaiswal95@gmail.com>
2022-04-05 12:27:29 +0530

    Initial commit

---
+++ components/sidebar.js
@@ -0,0 +1,10 @@
+function sidebar() {
+  // return your html component here
+  //Make sure to give input search box id as "searchbar"
+
+    return ` <h1>Blog Search</h1>
+    <input type="text" id="searchbar" placeholder="Search">`
+  
+}
+
+export default sidebar;
